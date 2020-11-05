5d54a4da25765f7d55565042dc73f7eefc672acc
netizenMx <cesar.martinez@leanbusinessg.com>
2020-11-05 10:28:13 -0600

    Commit de Entrega LBG

--- src/main/java/com/mx/lbg/bimbo/catalogosservice/CatalogosServiceApplication.java
+++ src/main/java/com/mx/lbg/bimbo/catalogosservice/CatalogosServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 /* Descripcion: Servicio Rest para el manejo de las tablas de catalogos de la aplicacion
  * F. Creacion: Septiembre 2020
  * Creado Por : Carlos Luna Fernandez  
+ * Lean Business Group
  */
 
 @SpringBootApplication
